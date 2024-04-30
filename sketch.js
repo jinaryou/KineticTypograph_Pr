973b7158bcaa08a24bb8b7a7664ba466b14dacfa
jinaryou <jryou@go.pasadena.edu>
2024-04-30 11:23:26 -0700

    Update sketch.js
    change sampleFactor and angle, add cos

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 // let fontBl;
 let font, points=[];
 let r = 25;
-let angle =0;
+let angle = 0;
+let csize = 10;
 
 function preload() {
   // fontBl = loadFont("fonts/Roboto-Black.ttf")
@@ -17,7 +18,7 @@ function setup() {
 
 
   points = font.textToPoints("PCC", 50, 300, 300, {
-    sampleFactor:0.2,
+    sampleFactor:0.1,
   simplifyThreshold:0});
 
   angleMode(DEGREES)
@@ -28,9 +29,15 @@ function draw() {
 
   // text("Hello", 20, 50);
 
+  // for (let i=0; i<points.length; i++) {
+  //   fill(random(255), random(255), random(255)),
+  //   ellipse(points[i].x + r*sin(angle + i*25), points[i].y, 5, random(5, 20))
+  // angle += 10;
+
   for (let i=0; i<points.length; i++) {
-    fill(random(255), random(255), random(255));
-    ellipse(points[i].x + r*sin(angle + i*25), points[i].y, 10, 10)
+    fill(random(0), random(255), random(255)),
+    ellipse(points[i].x+ r*cos(angle + i*5) , points[i].y+ r*sin(angle + i*5), 10, 10)
+    
   }
-  angle += 10;
+  angle += 1;
 }
