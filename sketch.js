fc1da6e791d8a515b0a4b8646af0a06f810d9ee8
jinaryou <jryou@go.pasadena.edu>
2024-05-02 11:49:20 -0700

    changed ellipse (from fill to noFill), text (from 'PCC' to 'JR') and cleared cos.

--- sketch.js
+++ sketch.js
@@ -1,25 +1,30 @@
-// let fontBl;
-let font, points=[];
+let font;
+let points = [];
+let bbox;
+
 let r = 25;
 let angle = 0;
-let csize = 10;
 
-function preload() {
-  // fontBl = loadFont("fonts/Roboto-Black.ttf")
-  font = loadFont("Roboto-Regular.ttf")
+
+// fonts
+function preload(){
+  font = loadFont("Roboto-Regular.ttf");
 }
 
 function setup() {
-  // frameRate(3)
-  createCanvas(700, 400);
-  // fill('magenta');
-  textSize(50);
+  createCanvas(600, 400);
+
+  const fsize = 300;
+
   textFont(font);
+  textSize(fsize);
+  // textAlign(CENTER);
 
+  points = font.textToPoints("JR", 0, 0, fsize, {
+    samplefactor: 0.1,
+  });
 
-  points = font.textToPoints("PCC", 50, 300, 300, {
-    sampleFactor:0.1,
-  simplifyThreshold:0});
+  bbox = font.textBounds("JR", 0, 0, fsize);
 
   angleMode(DEGREES)
 }
@@ -27,17 +32,27 @@ function setup() {
 function draw() {
   background(0);
 
-  // text("Hello", 20, 50);
+  push();
+  translate(width/2 - bbox.w/2, height/2 + bbox.h/3);
 
-  // for (let i=0; i<points.length; i++) {
-  //   fill(random(255), random(255), random(255)),
-  //   ellipse(points[i].x + r*sin(angle + i*25), points[i].y, 5, random(5, 20))
-  // angle += 10;
+  // noFill()
+  // stroke(255, 0, 0)
+  // rect(bbox.x, bbox.y, bbox.w, bbox.h)
+
+  noFill();
+  // stroke(255, 255, 0)
+  // points.forEach((pt, i) => {
+  //   stroke(i, random(255), 255);
+  //   ellipse(pt.x, pt.y, 40, 40);
+  // });
 
   for (let i=0; i<points.length; i++) {
-    fill(random(0), random(255), random(255)),
-    ellipse(points[i].x+ r*cos(angle + i*5) , points[i].y+ r*sin(angle + i*5), 10, 10)
-    
+    stroke(i, random(255), 255),
+    ellipse(points[i].x + r*cos(angle + i*5) , points[i].y, 50, 50)
   }
   angle += 1;
+  pop();
+
+  
+  
 }
---
+++ sketch_PCC.js
@@ -0,0 +1,43 @@
+// let fontBl;
+let font, points=[];
+let r = 25;
+let angle = 0;
+
+function preload() {
+  // fontBl = loadFont("fonts/Roboto-Black.ttf")
+  font = loadFont("Roboto-Regular.ttf")
+}
+
+function setup() {
+  // frameRate(3)
+  createCanvas(700, 400);
+  // fill('magenta');
+  textSize(50);
+  textFont(font);
+
+
+  points = font.textToPoints("PCC", 50, 300, 300, {
+    sampleFactor:0.1,
+  simplifyThreshold:0});
+
+  angleMode(DEGREES)
+}
+
+function draw() {
+  background(0);
+
+  // text("Hello", 20, 50);
+
+  // for (let i=0; i<points.length; i++) {
+  //   fill(random(255), random(255), random(255)),
+  //   ellipse(points[i].x + r*sin(angle + i*25), points[i].y, 5, random(5, 20))
+  // }
+  // angle += 10;
+
+  for (let i=0; i<points.length; i++) {
+    fill(random(0), random(255), random(255)),
+    ellipse(points[i].x+ r*cos(angle + i*5) , points[i].y+ r*sin(angle + i*5), 10, 10)
+    
+  }
+  angle += 1;
+}
