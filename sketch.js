7f706e66d2bbc76fa96a942569db11606a51c977
jinaryou <jryou@go.pasadena.edu>
2024-05-06 14:33:06 -0700

    Update sketch.js
    changed ellipse to rect and add the mouseIsPressed

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ let bbox;
 
 let r = 25;
 let angle = 0;
+let color;
 
 
 // fonts
@@ -33,26 +34,27 @@ function draw() {
   background(0);
 
   push();
-  translate(width/2 - bbox.w/2, height/2 + bbox.h/3);
+  translate(width/2 - bbox.w/1.5, height/2 + bbox.h/3);
 
   // noFill()
   // stroke(255, 0, 0)
   // rect(bbox.x, bbox.y, bbox.w, bbox.h)
 
   noFill();
-  // stroke(255, 255, 0)
-  // points.forEach((pt, i) => {
-  //   stroke(i, random(255), 255);
-  //   ellipse(pt.x, pt.y, 40, 40);
-  // });
+
+  if (mouseIsPressed) {
+    color = random(255);
+    fill(0, 50);
+    angle += 2;
+  } else {
+    color = 0
+  }
 
   for (let i=0; i<points.length; i++) {
-    stroke(i, random(255), 255),
-    ellipse(points[i].x + r*cos(angle + i*5) , points[i].y, 50, 50)
+    stroke(i, color, 255),
+    rect(points[i].x + r*cos(angle + i*5), points[i].y, 50, 50)
   }
   angle += 1;
   pop();
 
-  
-  
 }
